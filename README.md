016938b6a7fb33e28fcd7202670eab6cde83cc40
Jack <songjiapei@gmail.com>
2015-03-26 01:04:13 -0400

    - building single page events, formatting, nav bar, and content

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Personal site, based on jekyll, using purecss
 
 ####TODO:
 
+- add logo
+- add right colored icons
+- finish color scheme
+- add project screen shots
+- fix about formatting
 - add resume
 - add blog/posts content
--- js/index.js
+++ js/index.js
@@ -1,8 +1,62 @@
-(function() {
-  document.addEventListener("DOMContentLoaded", function (event) { 
-    var nav = document.getElementById('nav'),
-        head = new window.Headroom(nav);
+$(document).ready(function () {
+  $('#nav').headroom();
 
-    head.init();
+  String.prototype.testNavWidth = function(font) {
+    var f = font || '20px arial',
+        o = $('<a>' + this + '</a>')
+              .css({'position': 'absolute', 'float': 'left', 'white-space': 'nowrap', 'visibility': 'hidden', 'font': f})
+              .appendTo($('#nav ul li').first()),
+        w = o.width()+3;
+
+    o.remove();
+
+    return w;
+  }
+
+  function animateTextSwap(element, newText) {
+    element.stop().velocity({opacity: 0}, 100, 'linear', function () {
+      element.velocity({width: newText.testNavWidth()}, 100, 'linear', function () {
+        element.html(newText).velocity({opacity: 1}, 100, 'linear');
+      });
+    });
+  }
+
+  function animatePieceIn(element, onComplete) {
+    element.stop().css({display: 'block'});
+    element.stop().velocity({opacity: 1}, 200, 'linear', function () {
+      onComplete && onComplete();
+    });
+  }
+
+  function animatePieceOut(element, onComplete) {
+    element.stop().velocity({opacity: 0}, 200, 'linear', function () {
+      element.stop().css({display: 'none'});
+      onComplete && onComplete();
+    });
+  }
+
+  function animatePieceSwap(inElement, outElement) {
+    animatePieceOut(outElement, function () {
+      animatePieceIn(inElement);
+    });
+  }
+
+  $('#nav li a').on('click', function () {
+    element = $(this);
+    oldElement = $('.nav-current');
+
+    if (!element.hasClass('nav-current')) {
+      if(oldElement.length > 0){
+        oldElement.removeClass('nav-current');
+        animateTextSwap(oldElement, oldElement.attr('nav-notcurrent'));
+      }
+
+      element.addClass('nav-current');
+      animateTextSwap(element, element.attr('nav-current'));
+
+      animatePieceSwap($('#mainpage-' + element.attr('nav-current')), $('.mainpage-current'));
+      $('.mainpage-current').removeClass('mainpage-current');
+      $('#mainpage-' + element.attr('nav-current')).addClass('mainpage-current');
+    }
   });
-}());
+});
