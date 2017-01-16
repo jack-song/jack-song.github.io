d8480f6b85d8d612c206a0e8cd3e00ae375b570b
Jack Song <jackthesong@gmail.com>
2017-01-16 21:48:32 +0300

    rebuilding site Mon Jan 16 21:48:32 EAST 2017

--- js/blazy.js
+++ js/blazy.js
@@ -8,19 +8,6 @@
 (function() {
     // Initialize
     var bLazy = new Blazy({
-      offset: 400,
-      breakpoints: [{
-          width: 420,
-          src: 'data-src-small'
-      }], 
-      success: function(element){
-          setTimeout(function(){
-            // We want to remove the loader gif now.
-            // First we find the parent container
-            // then we remove the "loading" class which holds the loader image
-            var parent = element.parentNode;
-            parent.className = parent.className.replace(/\bloading\b/,'');
-          }, 200);
-      }
+      offset: 400
    });
 })();
