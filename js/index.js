a1e9f3eba90179e29c569c33fc9a421f21747d43
Jack Song <jackthesong@gmail.com>
2017-02-19 18:15:12 +0300

    rebuilding site Sun Feb 19 18:15:12 EAST 2017

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 (function() {
-  const ACTIONS = ["organize my notes", "iterate on some design work", "work on a painting", "take better photographs", "meditate", "listen to a new band",
-                "improve my rock climbing", "eat healthier", "work on a side project", "practice an instrument", "read a book", "sketch for a while", "find a lecture to watch", "look into a new technology"];
+  const ACTIONS = ["organize my notes", "iterate on some design work", "paint", "take better photographs", "meditate", "listen to a new band",
+                "improve my rock climbing", "eat healthier", "work on a side project", "practice an instrument", "read a book", "sketch", "find a lecture to watch", "look into a new technology"];
 
   const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -31,7 +31,8 @@
   
   document.addEventListener("DOMContentLoaded", function() {
     // replace the Date and action
-    document.getElementById('date').innerHTML = `${month + " " + day}: I'll ${action}.`;
+    document.getElementById('date').innerHTML = `${month + " " + day}`;
+    document.getElementById('action').innerHTML = `Today I'll ${action}`;
   });
 })();
 
@@ -39,8 +40,7 @@ window.yqlCallback = function (data) {
   function link (name, url) {
     return `<a href="${url}">${name}</a>`;
   }
-  let extra = document.getElementById('extra');
   let title = data.query.results.a.title;
   let url = 'https://en.wikipedia.org' + data.query.results.a.href;
-  extra.innerHTML = ` Today's quick read: <em>${link(title, url)}</em>.`;
+  document.getElementById('extra').innerHTML = `\ and read about: <em>${link(title, url)}</em>`;
 }
